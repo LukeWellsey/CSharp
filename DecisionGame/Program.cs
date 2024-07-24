0d9d6e77b7c7dc3a67b8e46b21e458e53b5ba21e
Luke Wells <46170241+LukeWellsey@users.noreply.github.com>
2024-07-23 19:47:48 -0600

    Game is functional

--- DecisionGame/Program.cs
+++ DecisionGame/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 
 class DecisionGame {
     static void Main() {
@@ -8,5 +6,112 @@ class DecisionGame {
         var data = JObject.Parse(jsonData);
 
         Console.WriteLine(data["intro"]);
+        string decision = GetUserPrompt(["torch","sword","enter","back"], data);
+        if (decision == "torch") {
+            ReadOutput(data, "story", "takeTorch");
+            decision = GetUserPrompt(["sword","enter","back"], data);
+            if (decision == "sword" || decision == "enter") {
+                //var is true if decision was "sword"
+                bool hasSword = decision == "sword";
+                ReadOutput(data, "story", "takeSword");
+                decision = GetUserPrompt(["enter","back"], data);
+                if (decision == "enter") {
+                    ReadOutput(data, "story", "seeCorridor");
+                    decision = GetUserPrompt(["walk","look"], data);
+                    if (decision == "walk") {
+                        ReadOutput (data, "endings", "becomeTrapped");
+                    } else if (decision == "look") {
+                        ReadOutput(data, "story", "avoidTrap");
+                        decision = GetUserPrompt(["continue","back"], data);
+                        if (decision == "continue") {
+                            ReadOutput(data, "story", "reachRoom");
+                            decision = GetUserPrompt(["approach","open"], data);
+                            if (decision == "approach") {
+                                ReadOutput(data, "story", "fallDown");
+                            } else if (decision == "open") {
+                                ReadOutput(data, "story", "fallDownChest");
+                            }
+                            if(hasSword) {
+                                decision = GetUserPrompt(["light","climb"], data);
+                                if (decision == "light") {
+                                    ReadOutput(data, "story", "holeClosed");
+                                    ReadOutput(data, "misc", "end");
+                                } else if (decision == "climb") {
+                                    ReadOutput(data, "story", "climbOut");
+                                    decision = GetUserPrompt(["approach","open"], data);
+                                    if (decision == "approach") {
+                                        ReadOutput(data, "endings", "curseBlind");
+                                        ReadOutput(data, "misc", "end");
+                                    } else if (decision == "open") {
+                                        ReadOutput(data, "story", "goldCoins");
+                                        ReadOutput(data, "endings", "congratulations");
+                                    }                              
+                                }
+                            } else {
+                                ReadOutput(data, "endings", "hole");
+                                ReadOutput(data, "misc", "end");
+                            }
+                        } else if (decision == "back") {
+                            ReadOutput(data, "endings", "claustrophobic");
+                            ReadOutput(data, "misc", "end");
+                        }
+                    }
+                } else if (decision == "back") {
+                    ReadOutput (data, "endings", "neatSword");
+                }
+
+            } else if (decision == "back") {
+                ReadOutput (data, "endings", "neatSword");
+            }
+        } else if (decision == "sword") {
+            ReadOutput(data, "endings", "swordNoLight");
+        } else if (decision == "enter") {
+            ReadOutput (data, "endings", "noLight");
+        } else if (decision == "back") {
+            ReadOutput (data, "endings", "nothingVentured");
+        } //no need for catch-all 'else' as the getUserPrompt method handles invalid selections and re-prompts user.
+    }
+    /*************************************************************
+    * getUserPrompt accepts a string array of valid options, as well 
+    * as the json file. The method loops through each of the valid  
+    * options and compares it with the user input it also collects,
+    * and the method returns the validated input, or reprompts the
+    * user if no valid input was located.
+    *************************************************************/
+    static string GetUserPrompt(string[] validOptions, JObject data) {
+        // Console.WriteLine("getUserPrompt starting");
+        while (true) {
+            //print out the valid options
+            if (data != null) {
+                foreach (string option in validOptions) {
+                    Console.WriteLine(data["actions"][option]);
+                }
+            }
+
+            //linebreak
+            Console.WriteLine("\n");
+            Console.WriteLine(data["misc"]["prompt"]);
+            //? denotes a nullable type, even though I check for non-null on the next line
+            string? input = Console.ReadLine();
+            if (input != null) {
+                input = input.ToLower();
+                foreach (string option in validOptions) {
+                    if (input == option) {
+                        return input;
+                    }
+                }
+            }
+            Console.WriteLine(data["misc"]["invalid"]);
+        }
+    }
+    /*******************************************************************
+    * ReadOutput handles repetitive data validation and console printing
+    ******************************************************************/
+    static void ReadOutput(JObject data, string path, string message) {
+        if (data != null && path != null && message != null) {
+            Console.WriteLine("");
+            Console.WriteLine(data[path][message]);
+            Console.WriteLine("");
+        }   
     }
 }
--- README.md
+++ README.md
@@ -2,27 +2,21 @@
 
 This project provides an opportunity for me to be creative and get to know the C Sharp language. I have heard of this language quite a lot in the past, and I've heard the term '.net' mentioned, but I've never had much exposure to the language at all before now. My goal for this project is to produce a working console based adventure game, with multiple different endings depending on your decisions throughout the game. I hope to learn the basics of C# syntax surrounding basic control flow through this project.
 
-{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
-
 [Software Demo Video](http://youtube.link.goes.here)
 
 # Development Environment
 
 This program was developed using the installed version of dotnet on my Windows PC (8.0.303), and VSCode with the C# Dev Kit extensions installed, using the C# programming language. 
 
-I used the System library to provide console output, with a JSON file storing the text for the game.
+I used the default System library to provide console output and collect user input, with a JSON file storing the text for the game. I used the Newtonsoft library to read the JSON file.
 
 # Useful Websites
 
-{Make a list of websites that you found helpful in this project}
-
 - [ChatGPT](https://chatgpt.com)
 - [C# Environment Configuration](https://www.geeksforgeeks.org/setting-environment-c-sharp/)
+- [C# - W3 Schools](https://www.w3schools.com/cs/index.php)
 
 # Future Work
 
-{Make a list of things that you need to fix, improve, and add in the future.}
-
-- Item 1
-- Item 2
-- Item 3
+- Id like to add data validation for the data in the JSON file to ensure null values are better handled on the off chance those are an issue.
+- logic needs fixed surrounding the beginning of the game if the sword is not selected, wrong prompt shows and causes a need to restructure the code, but this doesn't break the game.
